e543fded57d16b4dbbd2a8f1b725fdeff9ba269d
Francisco A. Lozano <flozano@gmail.com>
2024-03-25 15:45:24 +0100

    Credentials provider chain almost default; add logging

--- aws-eks-identity-pod-sample/src/main/java/io/github/flozano/eksidentitypod/sample/BucketListing.java
+++ aws-eks-identity-pod-sample/src/main/java/io/github/flozano/eksidentitypod/sample/BucketListing.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.auth.AWSCredentialsProviderChain;
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.auth.WebIdentityTokenCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.auth.profile.internal.ProfileAssumeRoleCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
@@ -22,8 +24,14 @@ class BucketListing {
 
 	BucketListing() {
 		client = AmazonS3Client.builder().withCredentials(
-				new AWSCredentialsProviderChain(new EKSIdentityPodCredentialsProvider(),
-						new InstanceProfileCredentialsProvider(), new ProfileCredentialsProvider())).build();
+				new AWSCredentialsProviderChain(
+						new EKSIdentityPodCredentialsProvider(), // added
+						new EnvironmentVariableCredentialsProvider(),
+						new SystemPropertiesCredentialsProvider(),
+						WebIdentityTokenCredentialsProvider.create(),
+						new ProfileCredentialsProvider(),
+						new EC2ContainerCredentialsProviderWrapper()
+				)).build();
 	}
 
 	public List<String> listBuckets() {
